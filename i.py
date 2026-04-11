9b4008389d6bd0a003dda141b6596caac8bfccde
bainbas <inbappm@gmail.com>
2026-04-11 11:26:23 +0530

    Basic Python Code

---
+++ i.py
@@ -0,0 +1,8 @@
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+print ("Name is",name)
+print ("Age is", age)
+if age >= 18:
+    print ("You're Major")
+else:
+    print ("You're Minor")
